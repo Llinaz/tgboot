10e5fb69a841f17e6d7391abb5b34feb36a54a2e
Linaz <zellik122711@mail.ru>
2024-07-03 10:49:19 +0800

    first commit

---
+++ src/main/java/ru/skillfactorydemo/tgbot/entity/ActiveChat.java
@@ -0,0 +1,17 @@
+package ru.skillfactorydemo.tgbot.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "ACTIVE-CHAT")
+public class ActiveChat {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "CHAT-ID")
+    private Long chatId;
+}
