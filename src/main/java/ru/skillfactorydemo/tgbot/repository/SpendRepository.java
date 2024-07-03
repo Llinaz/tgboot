2593d64d85aa8394bff53d363ebc027ee2d0fa1e
Linaz <zellik122711@mail.ru>
2024-07-03 10:51:47 +0800

    2 commit

--- src/main/java/ru/skillfactorydemo/tgbot/repository/SpendRepository.java
+++ src/main/java/ru/skillfactorydemo/tgbot/repository/SpendRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skillfactorydemo.tgbot.entity.Spend;
 
+
 @Repository
 public interface SpendRepository extends JpaRepository<Spend, Long> {
 }
